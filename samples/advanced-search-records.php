a861923ff2e581a2b4d4d3a5ee8bf8aec6434898
Mark Headd <mheadd@gmail.com>
2016-07-26 10:16:20 -0400

    Fixes #13

--- samples/advanced-search-records.php
+++ samples/advanced-search-records.php
@@ -5,9 +5,14 @@ require '../src/Construct.php';
 // App, agency & environment settings.
 require '../config.php';
 
+$customID = '';
+
+$params = array("limit" => 1000, "expand" => 'addresses,professionals');
+$data = array("customId" => $customID, "offset" => 0, "module" => 'Building');
+
 try {
-	$rec = new Records($app_id, $app_secret, null, $environment, $agency_name);
-	$response = $rec->getRecords('/v4/records', AuthType::$NoAuth, array("limit" => 1), true);
+	$rec = new Records($app_id, $app_secret, $token, $environment, $agency);
+	$response = $rec->advancedSearchRecords('/v4/search/records', AuthType::$AccessToken, $params, $data, true, true);
 	echo json_encode($response); // To output JSON string.
 }
 catch(ConstructException $ex) {
